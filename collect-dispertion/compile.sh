8f8d1b7295cad860c89d8d2ce1986e9f2304b926
Maria Sosnovskaya <msosnovskaya@lineate.com>
2021-06-05 22:33:47 +0600

    added collect-dispertion

---
+++ collect-dispertion/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dpcpp -DMKL_ILP64 -I${MKLROOT}/include ./DispertionCollector.cpp -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl
+
+chmod +x a.out
