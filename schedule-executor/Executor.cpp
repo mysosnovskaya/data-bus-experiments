59539efa77f84c89de95964c582af50ff81c1ba5
Anton Malakhov <Anton.Malakhov@intel.com>
2021-10-02 16:27:35 +0300

    Fixed coreNumbers and position of barrirer

--- schedule-executor/Executor.cpp
+++ schedule-executor/Executor.cpp
@@ -25,7 +25,10 @@ vector<int> coresNumbers = {
      0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
     10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
     40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-    50, 51, 52, 53, 54, 55, 56, 57, 58, 59
+    50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+    60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
+    70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+    80, 81, 82, 83, 84, 85, 86, 87, 88, 89
 };
 
 const int iterationCount = 30;
@@ -97,16 +100,12 @@ vector<long> run() {
             CPU_ZERO(&cpuset);
             CPU_SET(coresNumbers[k], &cpuset);
             pthread_setaffinity_np(threads[k].native_handle(), sizeof(cpu_set_t), &cpuset);
-            cerr << "therad " << k << " created on core " << coresNumbers[k] << endl;
+            cerr << "thread " << k << " created on core " << coresNumbers[k] << endl;
+            threads[k].detach();
         }
-
-        high_resolution_clock::time_point startTime = high_resolution_clock::now();
 
         pthread_barrier_wait(&barrier);
-
-        for (int i = 0; i < threads.size(); i++) {
-            threads[i].detach();
-        }
+        high_resolution_clock::time_point startTime = high_resolution_clock::now();
 
         std::unique_lock<std::mutex> lock(m);
         cv.wait(lock, [] {return allJobsAreFinished(); });
--- schedule-executor/compile.sh
+++ schedule-executor/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dpcpp -DMKL_ILP64 -I${MKLROOT}/include ./Executor.cpp -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl
+dpcpp -std=c++17 -DMKL_ILP64 -I${MKLROOT}/include ./Executor.cpp -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl
 
 chmod +x a.out
