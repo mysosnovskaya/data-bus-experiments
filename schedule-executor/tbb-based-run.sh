c3f4e30647cd98fd84a90864291a35e92286647c
Maxim Sakhno <maxim.sakhno@gmail.com>
2021-10-10 23:08:52 +0600

    fix tbb-based-run.sh

--- schedule-executor/tbb-based-run.sh
+++ schedule-executor/tbb-based-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dpcpp -std=c++17 -DMKL_ILP64 -I${MKLROOT}/include ./TbbBasedExecutor.cpp -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl
+dpcpp -std=c++17 -DMKL_ILP64 -I${MKLROOT}/include ./TbbBasedExecutor.cpp -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -ltbb -lpthread -lm -ldl -o a-tbb.out
 
 chmod +x a-tbb.out
 
