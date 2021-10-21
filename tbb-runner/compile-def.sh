8c831433ac30e713702feca513e8e2a513403030
Anton Malakhov <Anton.Malakhov@intel.com>
2021-10-21 08:44:59 +0300

    Fixed affinity mask setting, added debug info and optimization flag

--- tbb-runner/compile-def.sh
+++ tbb-runner/compile-def.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-g++ -std=c++17 ./TbbRunner.cpp -lblas64 -llapacke64 -ltbb -lpthread -lm -ldl
-
-chmod +x a.out
+g++ -g -O3 -std=c++17 ./TbbRunner.cpp -lblas64 -llapacke64 -ltbb -lpthread -lm -ldl && chmod +x a.out
--- tbb-runner/compile-mkl.sh
+++ tbb-runner/compile-mkl.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-dpcpp -DMKL_ILP64 -I${MKLROOT}/include ./TbbRunner.cpp -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -ltbb -lpthread -lm -ldl
-
-chmod +x a.out
+dpcpp -g -O3 -DMKL_ILP64 -I${MKLROOT}/include ./TbbRunner.cpp -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -ltbb -lpthread -lm -ldl && chmod +x a.out
