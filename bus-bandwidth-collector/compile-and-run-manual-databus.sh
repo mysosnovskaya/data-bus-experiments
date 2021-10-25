7c90aa3725f522bcaf55ef281d31bad6dfade62c
Anton Malakhov <Anton.Malakhov@intel.com>
2021-10-25 17:55:55 +0300

    improved command line args

--- bus-bandwidth-collector/compile-and-run-manual-databus.sh
+++ bus-bandwidth-collector/compile-and-run-manual-databus.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-dpcpp -DMKL_ILP64 -I${MKLROOT}/include ./CalculateBusPercent.cpp -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl -o ManualCollector.out
+dpcpp -g -O3 -DMKL_ILP64 -I${MKLROOT}/include ./CalculateBusPercent.cpp -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl -o ManualCollector.out && chmod +x ManualCollector.out
 
-chmod +x ManualCollector.out
-
-sudo numactl -m 1 -N 1 ./ManualCollector.out
+numactl -m 1 -N 1 ./ManualCollector.out
--- schedule-executor/tbb-based-run.sh
+++ schedule-executor/tbb-based-run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -x  #echo on
 
-dpcpp -std=c++17 -DMKL_ILP64 -I${MKLROOT}/include ./TbbBasedExecutor.cpp -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -ltbb -lpthread -lm -ldl -o a-tbb.out
-
-chmod +x a-tbb.out
+dpcpp -g -O3 -std=c++17 -DMKL_ILP64 -I${MKLROOT}/include ./TbbBasedExecutor.cpp -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -ltbb -lpthread -lm -ldl -o a-tbb.out && chmod +x a-tbb.out
 
 numactl -m 1 -N 1 ./a-tbb.out 2> debug.txt
